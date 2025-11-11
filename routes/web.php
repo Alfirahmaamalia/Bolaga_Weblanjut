4b2de0fafb12a5482b5855fa425def213f68b2b4
AlfiRahmaAmalia <alfiaml08@gmail.com>
2025-11-11 21:14:41 +0700

    hapus beranda, benerin dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,6 @@ Route::get('/', function () {
 });
 
 
-// Beranda / Pencarian Lapangan
-Route::get('/beranda', function () {
-    return view('beranda');
-});
-
 // Authentication Routes
 Route::middleware('guest')->group(function () {
     Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
